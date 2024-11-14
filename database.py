19b9bb6df3d57992bcba31099ba4501ea28bd449
Maxim Sytdikov <maxim.sytdikov@saritasa.com>
2024-11-14 16:50:28 +0500

    Implement documentation for SquareRepository

--- database.py
+++ database.py
@@ -1,21 +1,26 @@
 from square import Square
 
 class SquareRepository:
+    """Repository with instance managing methods for `Square`."""
     def __init__(self):
         self.data = {}
         self.index = 0
 
     def add(self, square: Square) -> None:
+        """Add and return `Square` instance."""
         self.index += 1
         self.data[self.index] = square
         return square
 
     def remove(self, index: int) -> None:
+        """Remove `Square` instance and return index."""
         del self.data[index]
         return index
 
     def get(self, index: int) -> Square:
+        """Return `Square` instance."""
         return self.data[index]
 
     def get_all(self) -> dict[int: Square]:
+        """Return all `Square` instances."""
         return self.data
